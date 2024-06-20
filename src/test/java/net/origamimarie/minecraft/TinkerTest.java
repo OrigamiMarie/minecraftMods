9dbadb6fec93b713ca4e40712fc908e09f23b4fd
Marie Tessier <origamimarie@gmail.com>
2024-06-19 23:05:30 -0500

    Fix compile fail

--- src/test/java/net/origamimarie/minecraft/TinkerTest.java
+++ src/test/java/net/origamimarie/minecraft/TinkerTest.java
@@ -1,18 +1,11 @@
 package net.origamimarie.minecraft;
 
-import net.origamimarie.minecraft.util.ResourceCopyTransformer;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import static net.origamimarie.minecraft.util.ResourceCopyTransformer.TransformParameters.UP_ONE_FOLDER;
-
-public class Tinker {
+public class TinkerTest {
 
     @Test
     public void foo() {
