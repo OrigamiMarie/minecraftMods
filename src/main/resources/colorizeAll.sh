a189566cbed23803f43d758c400e92f886131043
Marie Tessier <origamimarie@gmail.com>
2024-06-19 22:54:55 -0500

    Applied the new ResourceCopyTransformer globally

--- src/main/resources/colorizeAll.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILES=`cat $1`
-
-for FILE in $FILES; do
-  ./colorize.sh colors $FILE
-done
-
--- src/test/java/net/origamimarie/minecraft/Tinker.java
+++ src/test/java/net/origamimarie/minecraft/Tinker.java
@@ -1,23 +1,35 @@
 package net.origamimarie.minecraft;
 
+import net.origamimarie.minecraft.util.ResourceCopyTransformer;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static net.origamimarie.minecraft.util.ResourceCopyTransformer.TransformParameters.UP_ONE_FOLDER;
+
 public class Tinker {
 
     @Test
     public void foo() {
-        List<String> colors = List.of("magenta", "red", "orange", "yellow", "lime", "cyan", "blue", "purple");
-        List<String> moreColors = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            moreColors.addAll(colors);
-        }
-        Collections.shuffle(moreColors);
-        System.out.println(moreColors.stream().map(c -> "\"" + c + "\"").collect(Collectors.joining(", ")));
+        System.out.println(mashFileParts(new File("hello/world/../hey")));
     }
+
+    private static File mashFileParts(File file) {
+        String fullFile = file.getAbsolutePath();
+        System.out.println(fullFile);
+        while (fullFile.contains("..")) {
+            int location = fullFile.indexOf("..");
+            System.out.println(location);
+            int startOfFileAbove = StringUtils.lastIndexOf(fullFile, "\\", location - 2);
+            System.out.println(startOfFileAbove);
+            fullFile = fullFile.substring(0, startOfFileAbove) + fullFile.substring(location + 2);
+        }
+        return new File(fullFile);
+    }
+
 }
-// "red", "lime", "yellow", "blue", "purple", "blue", "orange", "lime", "magenta", "cyan", "orange", "magenta", "orange", "red", "cyan", "purple", "lime", "cyan", "blue", "purple", "blue", "red", "yellow", "cyan", "yellow", "magenta", "orange", "yellow", "red", "lime", "magenta", "purple"
